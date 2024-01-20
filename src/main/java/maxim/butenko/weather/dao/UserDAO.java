4e6d99be17f9eed33429006fe6924618519ff95c
MaxemsSpaghetti <maxbuten00@mail.ru>
2024-01-20 22:29:53 +0300

    fix: update UserDAO

--- src/main/java/maxim/butenko/weather/dao/UserDAO.java
+++ src/main/java/maxim/butenko/weather/dao/UserDAO.java
@@ -1,56 +1,60 @@
 package maxim.butenko.weather.dao;
 
 import lombok.extern.slf4j.Slf4j;
+import maxim.butenko.weather.entity.Role;
 import maxim.butenko.weather.entity.User;
 import maxim.butenko.weather.utils.HibernateConnection;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 
-import java.util.Objects;
 import java.util.Optional;
 @Slf4j
-public class UserDAOImpl {
+public class UserDAO {
 
-    private static final UserDAOImpl INSTANCE = new UserDAOImpl();
+    private static final UserDAO INSTANCE = new UserDAO();
 
     private final SessionFactory sessionFactory = HibernateConnection.getConnection();
 
-    private UserDAOImpl() {
+    private UserDAO() {
 
     }
 
-    public static UserDAOImpl getInstance() {
+    public static UserDAO getInstance() {
         return INSTANCE;
     }
 
-    public void add(User user) {
+    public Optional<User> add(User user) {
         Session session = sessionFactory.getCurrentSession();
-        try (session) {
+        try {
+
             session.beginTransaction();
             session.persist(user);
             session.getTransaction().commit();
+
+            return Optional.of(user);
+
         } catch (Exception e) {
             log.error("An error occurred while adding user: {}", e.getMessage(), e);
             log.error("Stack trace: ", e);
             session.getTransaction().rollback();
         }
+        return Optional.empty();
     }
-    public Optional<User> findByLoginAndPassword(String login, String password) {
+    public Optional<User> findByLogin(String login) {
         Session session = sessionFactory.getCurrentSession();
-        try (session) {
+        try {
             session.beginTransaction();
 
-            Query<User> query = session.createQuery("from User where login = :login and password = :password");
+            Query<User> query = session.createQuery("SELECT u FROM User u WHERE login = :login", User.class);
 
             query.setParameter("login", login);
-            query.setParameter("password", password);
-            session.getTransaction().commit();
             User user = query.uniqueResult();
 
-            if (user != null && Objects.equals(user.getPassword(), password)) {
-                return Optional.of(user);
-            }
+            session.getTransaction().commit();
+
+            user.setRole(Role.USER);
+            return Optional.of(user);
 
         } catch (Exception e) {
             log.error("An error occurred while processing login and password: {}", e.getMessage(), e);
