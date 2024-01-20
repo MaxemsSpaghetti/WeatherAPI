902e4de72ecedfb8b7b5a55bc8390a181e3d20da
MaxemsSpaghetti <maxbuten00@mail.ru>
2024-01-20 22:29:39 +0300

    fix: update id

--- src/main/java/maxim/butenko/weather/dto/UserDTO.java
+++ src/main/java/maxim/butenko/weather/dto/UserDTO.java
@@ -1,13 +1,16 @@
 package maxim.butenko.weather.dto;
 
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 import maxim.butenko.weather.entity.Role;
+
+import java.math.BigInteger;
 
 @Value
 @Builder
 public class UserDTO {
-    Long id;
+    BigInteger id;
     String login;
     String password;
     Role role;
