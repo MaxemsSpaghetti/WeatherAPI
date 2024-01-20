f2479db3da9c861db46cf3b62c6e3de2a20b61bd
MaxemsSpaghetti <maxbuten00@mail.ru>
2024-01-20 22:28:21 +0300

    fix: update WeatherSessionService

--- src/main/java/maxim/butenko/weather/service/WeatherSessionService.java
+++ src/main/java/maxim/butenko/weather/service/WeatherSessionService.java
@@ -1,20 +1,24 @@
 package maxim.butenko.weather.service;
 
 import maxim.butenko.weather.dao.SessionDAO;
+import maxim.butenko.weather.dto.UserDTO;
 import maxim.butenko.weather.dto.WeatherSessionDTO;
+import maxim.butenko.weather.entity.User;
 import maxim.butenko.weather.entity.WeatherSession;
-import org.modelmapper.ModelMapper;
+import maxim.butenko.weather.utils.CustomMapper;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
+
+import static org.modelmapper.Converters.Collection.map;
 
 public class WeatherSessionService {
 
    private final SessionDAO sessionDAO = SessionDAO.getInstance();
     private static final WeatherSessionService INSTANCE = new WeatherSessionService();
 
-    private static final ModelMapper modelMapper = new ModelMapper();
+    private static final CustomMapper customMapper = CustomMapper.getInstance();
 
     public static WeatherSessionService getInstance() {
         return INSTANCE;
@@ -24,23 +28,26 @@ public class WeatherSessionService {
 
     }
 
-    public Optional<WeatherSessionDTO> add(Long userId) {
-        WeatherSession session = buildWeatherSession(UUID.randomUUID(), userId, LocalDateTime.now().plusDays(1));
-        return sessionDAO.add(session).map(weatherSession -> modelMapper.map(weatherSession, WeatherSessionDTO.class));
+    public Optional<WeatherSessionDTO> add(UserDTO userDTO) {
+        User user = customMapper.convert(userDTO, User.class);
+        WeatherSession session = buildWeatherSession(UUID.randomUUID(), user, LocalDateTime.now().plusDays(1));
+        Optional<WeatherSession> weatherSession = sessionDAO.add(session);
+        return Optional.ofNullable(weatherSession).map(weatherSes -> customMapper.convert(weatherSes, WeatherSessionDTO.class));
     }
 
     public Optional<WeatherSessionDTO> findById(UUID id) {
-        return sessionDAO.findById(id).map(weatherSession -> modelMapper.map(weatherSession, WeatherSessionDTO.class));
+        return sessionDAO.findById(id)
+                .map(weatherSession -> customMapper.convert(weatherSession, WeatherSessionDTO.class));
     }
 
     public void delete(UUID id) {
         sessionDAO.delete(id);
     }
 
-    private WeatherSession buildWeatherSession(UUID id, Long userId, LocalDateTime expiresAt) {
+    private WeatherSession buildWeatherSession(UUID id, User user, LocalDateTime expiresAt) {
         return WeatherSession.builder()
                 .id(id)
-                .userId(userId)
+                .user(user)
                 .expiresAt(expiresAt)
                 .build();
     }
