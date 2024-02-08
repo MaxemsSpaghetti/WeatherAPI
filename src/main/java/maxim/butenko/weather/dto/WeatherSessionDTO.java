c3139b4b7645b26eaf962a06da4f4bd213297af8
MaxemsSpaghetti <maxbuten00@mail.ru>
2024-02-08 20:59:10 +0300

    fix: add constructors to DTO

--- src/main/java/maxim/butenko/weather/dto/WeatherSessionDTO.java
+++ src/main/java/maxim/butenko/weather/dto/WeatherSessionDTO.java
@@ -1,6 +1,8 @@
 package maxim.butenko.weather.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 import maxim.butenko.weather.entity.User;
 
@@ -8,6 +10,8 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Value
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
 @Builder
 public class WeatherSessionDTO {
     UUID id;
