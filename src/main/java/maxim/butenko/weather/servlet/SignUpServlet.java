fec2ad7cf10a249bc6c3d2818c3a5b974116e9a7
MaxemsSpaghetti <maxbuten00@mail.ru>
2024-02-08 21:02:53 +0300

    fix: add cookie handler instead of creating a new cookie in servlet

--- src/main/java/maxim/butenko/weather/servlet/SignUpServlet.java
+++ src/main/java/maxim/butenko/weather/servlet/SignUpServlet.java
@@ -5,7 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import maxim.butenko.weather.dto.UserDTO;
 import maxim.butenko.weather.dto.WeatherSessionDTO;
 import maxim.butenko.weather.service.UserService;
-import maxim.butenko.weather.service.WeatherSessionService;
+import maxim.butenko.weather.service.SessionService;
+import maxim.butenko.weather.util.CookieHandler;
 import maxim.butenko.weather.util.HtmlHelper;
 import maxim.butenko.weather.util.UrlPath;
 
@@ -19,13 +20,14 @@ import java.io.IOException;
 import java.util.Optional;
 
 @Slf4j
-@WebServlet(UrlPath.REGISTRATION)
-public class RegistrationServlet extends HttpServlet {
+@WebServlet(UrlPath.SIGN_UP)
+public class SignUpServlet extends HttpServlet {
 
     private final UserService userService = UserService.getInstance();
 
-    private final WeatherSessionService sessionService = WeatherSessionService.getInstance();
+    private final SessionService sessionService = SessionService.getInstance();
 
+    private final CookieHandler cookieHandler = CookieHandler.getInstance();
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String login = req.getParameter("login");
@@ -41,23 +43,24 @@ public class RegistrationServlet extends HttpServlet {
 
             if (optionalWeatherSessionDTO.isPresent()) {
 
-                WeatherSessionDTO session = optionalWeatherSessionDTO.get();
-                log.info("Session created: {}", session.getId());
+                WeatherSessionDTO sessionDTO = optionalWeatherSessionDTO.get();
+                log.info("Session created: {}", sessionDTO.getId());
 
-                Cookie cookie = new Cookie("session", session.getId().toString());
+
+                Cookie cookie = cookieHandler.createSessionCookie(sessionDTO);
                 resp.addCookie(cookie);
                 log.info("Cookie created: {}", cookie.getValue());
 
                 log.info("User has successfully registered: {}", login);
-                resp.sendRedirect(HtmlHelper.getHtmlPath(UrlPath.WEATHER));
+                resp.sendRedirect(HtmlHelper.getHtmlPath(UrlPath.AUTH_MAIN_PAGE));
             }
         } else {
-            resp.sendRedirect(HtmlHelper.getHtmlPath(UrlPath.REGISTRATION));
+            resp.sendRedirect(HtmlHelper.getHtmlPath(UrlPath.SIGN_UP));
         }
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher(HtmlHelper.getHtmlPath(UrlPath.REGISTRATION)).forward(req, resp);
+        req.getRequestDispatcher(HtmlHelper.getHtmlPath(UrlPath.SIGN_UP)).forward(req, resp);
     }
 }
