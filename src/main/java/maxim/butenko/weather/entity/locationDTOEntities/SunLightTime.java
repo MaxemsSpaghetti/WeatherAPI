6c786ad39fe63e0d5c54faf6e7e3e1865a321db2
MaxemsSpaghetti <maxbuten00@mail.ru>
2024-02-08 20:56:06 +0300

    feat: forgot about another class of LocationDTO

---
+++ src/main/java/maxim/butenko/weather/entity/locationDTOEntities/SunLightTime.java
@@ -0,0 +1,16 @@
+package maxim.butenko.weather.entity.locationDTOEntities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SunLightTime {
+
+    @JsonProperty("sunrise")
+    private Integer sunrise;
+
+    @JsonProperty("sunset")
+    private Integer sunset;
+}
